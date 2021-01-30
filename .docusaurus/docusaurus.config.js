608c7fc5179d191dab823cbbb2dc0201e51f039d
Nicolas Brown <nclsbrwn@gmail.com>
2021-01-29 19:38:04 -0500

    add v2 docs

---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,139 @@
+export default {
+  "title": "Samelogic Docs",
+  "tagline": "",
+  "url": "https://your-docusaurus-test-site.com",
+  "baseUrl": "/",
+  "onBrokenLinks": "throw",
+  "onBrokenMarkdownLinks": "warn",
+  "favicon": "img/favicon.ico",
+  "organizationName": "facebook",
+  "projectName": "docusaurus",
+  "themeConfig": {
+    "navbar": {
+      "title": "Samelogic Docs",
+      "logo": {
+        "alt": "Samelogic Logo",
+        "src": "img/logo.svg"
+      },
+      "items": [
+        {
+          "to": "docs/",
+          "activeBasePath": "docs",
+          "label": "Docs",
+          "position": "left"
+        },
+        {
+          "to": "blog",
+          "label": "Blog",
+          "position": "left"
+        },
+        {
+          "href": "https://github.com/facebook/docusaurus",
+          "label": "GitHub",
+          "position": "right"
+        }
+      ],
+      "hideOnScroll": false
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Style Guide",
+              "to": "docs/"
+            },
+            {
+              "label": "Second Doc",
+              "to": "docs/doc2/"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Stack Overflow",
+              "href": "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              "label": "Discord",
+              "href": "https://discordapp.com/invite/docusaurus"
+            },
+            {
+              "label": "Twitter",
+              "href": "https://twitter.com/docusaurus"
+            }
+          ]
+        },
+        {
+          "title": "More",
+          "items": [
+            {
+              "label": "Blog",
+              "to": "blog"
+            },
+            {
+              "label": "GitHub",
+              "href": "https://github.com/facebook/docusaurus"
+            }
+          ]
+        }
+      ],
+      "copyright": "Copyright © 2021 My Project, Inc. Built with Docusaurus."
+    },
+    "colorMode": {
+      "defaultMode": "light",
+      "disableSwitch": false,
+      "respectPrefersColorScheme": false,
+      "switchConfig": {
+        "darkIcon": "🌜",
+        "darkIconStyle": {},
+        "lightIcon": "🌞",
+        "lightIconStyle": {}
+      }
+    },
+    "docs": {
+      "versionPersistence": "localStorage"
+    },
+    "metadatas": [],
+    "prism": {
+      "additionalLanguages": []
+    },
+    "hideableSidebar": false
+  },
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "/home/nicolas/Desktop/docs/sidebars.js",
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        "blog": {
+          "showReadingTime": true,
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/blog/"
+        },
+        "theme": {
+          "customCss": "/home/nicolas/Desktop/docs/src/css/custom.css"
+        }
+      }
+    ]
+  ],
+  "baseUrlIssueBanner": true,
+  "i18n": {
+    "defaultLocale": "en",
+    "locales": [
+      "en"
+    ],
+    "localeConfigs": {}
+  },
+  "onDuplicateRoutes": "warn",
+  "customFields": {},
+  "plugins": [],
+  "themes": [],
+  "titleDelimiter": "|",
+  "noIndex": false
+};
