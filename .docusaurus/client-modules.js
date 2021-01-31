b484132025a36252ae1e0f98bf839c6a0314acd9
Shawn Mclean <shawnrmclean@gmail.com>
2021-01-30 21:12:18 -0500

    Move blog to garbage.

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/nicolas/Desktop/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/nicolas/Desktop/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/nicolas/Desktop/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/nicolas/Desktop/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/nicolas/Desktop/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/nicolas/Desktop/docs/src/css/custom.css"),
-];
