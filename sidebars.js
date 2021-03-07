bbb026ca236727ed25db13c777d2a3d91a38abba
dwayne <dwayne@samelogic.com>
2021-03-06 23:20:33 -0500

    Sidebar updated to show correct structure for docs

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
     Introduction: ['overview/getting-started'],
-    Tutorials: ['microsurvey/product/getting-started'],
-    "How To Guides": ['microsurvey/library/getting-started'],
-    "Release Notes": ['microsurvey/library/getting-started']
+    Tutorials: ['sections/product/getting-started'],
+    "How To Guides": ['sections/library/getting-started'],
+    "Release Notes": ['sections/release_notes/getting-started']
   },
 };
