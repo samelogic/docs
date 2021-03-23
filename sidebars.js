1d37d6dbe1759e385741e3ea1efbc05562214784
Shawn Mclean <shawnrmclean@gmail.com>
2021-03-23 14:17:04 -0600

    Add initial skeleton.

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,21 @@ module.exports = {
     Introduction: ['overview/getting-started'],
     Tutorials: ['sections/tutorials/getting-started'],
     "How To Guides": ['sections/howto/getting-started'],
+    Workflows: [
+      'workflows/overview',
+      {
+        Triggers: [
+          'workflows/triggers/overview', 
+          'workflows/triggers/page_load'
+        ]
+      }
+    ],
+    Microsurveys: [
+      'microsurveys/overview', 
+      {
+        Fields: ['microsurveys/fields/long_text']
+      },
+    ],
     "Release Notes": ['sections/release_notes/releases']
   },
 };
