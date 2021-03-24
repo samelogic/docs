c96f7d1a23895853e211b6f9ed264332213ae643
Shawn Mclean <shawnmclean@users.noreply.github.com>
2021-03-23 22:01:17 -0500

    Add initial skeleton. (#17)
    * Add initial skeleton.
    
    * Updte microsurvey overview.
    
    * Update churn
    
    * Update churn header.
    
    * Update churn title.
    
    * Update user churn sidebar label.
    
    * Update churn slug.

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,23 @@
 module.exports = {
   someSidebar: {
     Introduction: ['overview/getting-started'],
-    Tutorials: ['sections/tutorials/getting-started'],
+    Tutorials: ['tutorials/getting-started', 'tutorials/churn/index'],
     "How To Guides": ['sections/howto/getting-started'],
+    Workflows: [
+      'workflows/overview',
+      {
+        Triggers: [
+          'workflows/triggers/overview', 
+          'workflows/triggers/page_load'
+        ]
+      }
+    ],
+    Microsurveys: [
+      'microsurveys/overview', 
+      {
+        Fields: ['microsurveys/fields/long_text']
+      },
+    ],
     "Release Notes": ['sections/release_notes/releases']
   },
 };
