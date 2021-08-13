365ab59fb8f12ae2c297117e5375e7d6f23ddf5f
Shawn Mclean <shawnrmclean@gmail.com>
2021-08-13 00:21:44 -0400

    Remove sections.

--- sidebars.js
+++ sidebars.js
@@ -1,26 +1,29 @@
 module.exports = {
   someSidebar: {
-    Introduction: ['overview/getting-started'],
-    Tutorials: ['tutorials/getting-started', 'tutorials/churn/index'],
-    "How To Guides": ['sections/howto/getting-started'],
+    Introduction: ["overview/getting-started"],
+    Tutorials: ["tutorials/getting-started", "tutorials/churn/index"],
+    "How To Guides": ["howto/getting-started"],
     Workflows: [
-      'workflows/overview',
+      "workflows/overview",
       {
         Triggers: [
-          'workflows/triggers/overview',
-          'workflows/triggers/filters', 
-          'workflows/triggers/page_view',
-          'workflows/triggers/click_on',
-          'workflows/triggers/hover_on'
-        ]
-      }
-    ],
-    Microsurveys: [
-      'microsurveys/overview', 
-      {
-        Fields: ['microsurveys/fields/long_text', 'microsurveys/fields/multiple_choice']
+          "workflows/triggers/overview",
+          "workflows/triggers/filters",
+          "workflows/triggers/page_view",
+          "workflows/triggers/click_on",
+          "workflows/triggers/hover_on",
+        ],
       },
     ],
-    "Release Notes": ['sections/release_notes/releases']
+    Microsurveys: [
+      "microsurveys/overview",
+      {
+        Fields: [
+          "microsurveys/fields/long_text",
+          "microsurveys/fields/multiple_choice",
+        ],
+      },
+    ],
+    "Release Notes": ["release_notes/releases"],
   },
 };
