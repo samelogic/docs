862fb6095598179eccc703de8ca262bf14631628
Shawn Mclean <shawnrmclean@gmail.com>
2021-08-20 02:10:52 -0500

    Add advanced docs.

--- sidebars.js
+++ sidebars.js
@@ -14,6 +14,7 @@ module.exports = {
             collapsed: false,
             items: [
               "developers/sdks/web/introduction",
+              "developers/sdks/web/advanced",
               "developers/sdks/web/reference/samelogic-js",
               {
                 type: "link",
