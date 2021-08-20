c05c1246c70f26ed4c20f502c547716802389eba
Shawn Mclean <shawnrmclean@gmail.com>
2021-08-20 02:15:54 -0500

    Move Developers lower on the sidebar.

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,30 @@
 module.exports = {
   someSidebar: {
     Introduction: ["overview/getting-started"],
+    Tutorials: ["tutorials/getting-started", "tutorials/churn/index"],
+    "How To Guides": ["howto/getting-started"],
+
+    Workflows: [
+      "workflows/overview",
+      {
+        Triggers: [
+          "workflows/triggers/overview",
+          "workflows/triggers/filters",
+          "workflows/triggers/page_view",
+          "workflows/triggers/click_on",
+          "workflows/triggers/hover_on",
+        ],
+      },
+    ],
+    Microsurveys: [
+      "microsurveys/overview",
+      {
+        Fields: [
+          "microsurveys/fields/long_text",
+          "microsurveys/fields/multiple_choice",
+        ],
+      },
+    ],
     Developers: [
       "developers/overview",
       {
@@ -23,29 +47,6 @@ module.exports = {
               },
             ],
           },
-        ],
-      },
-    ],
-    Tutorials: ["tutorials/getting-started", "tutorials/churn/index"],
-    "How To Guides": ["howto/getting-started"],
-    Workflows: [
-      "workflows/overview",
-      {
-        Triggers: [
-          "workflows/triggers/overview",
-          "workflows/triggers/filters",
-          "workflows/triggers/page_view",
-          "workflows/triggers/click_on",
-          "workflows/triggers/hover_on",
-        ],
-      },
-    ],
-    Microsurveys: [
-      "microsurveys/overview",
-      {
-        Fields: [
-          "microsurveys/fields/long_text",
-          "microsurveys/fields/multiple_choice",
         ],
       },
     ],
