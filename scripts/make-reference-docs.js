cde8d9747b697f198ba67c20a7f0a25703662e68
Shawn Mclean <shawnrmclean@gmail.com>
2021-08-20 00:21:19 -0500

    make-reference converts markdown to have correct headers.

--- scripts/make-reference-docs.js
+++ scripts/make-reference-docs.js
@@ -1,8 +1,11 @@
+const { readdir, createReadStream, writeFile } = require("fs-extra");
+const { createInterface } = require("readline");
 const { join, parse } = require("path");
 const { exec } = require("child_process");
 
 // This scripot is used to generate the API documentation for libraries
 // generated with the @microsoft/api-generator library.
+// This script takes inspiration from: https://github.com/faastjs/faast.js/blob/master/build/make-docs.js
 
 const jsonDocs = [
   {
@@ -13,7 +16,7 @@ const jsonDocs = [
 
 async function main() {
   jsonDocs.forEach(async (doc) => {
-    console.log(`Generating docs for ${doc}`);
+    console.log(`Generating docs for ${doc.input}`);
     await new Promise((resolve, reject) =>
       exec(
         `api-documenter markdown --input-folder ${doc.input} --output-folder ${doc.output}`,
@@ -28,7 +31,73 @@ async function main() {
         }
       )
     );
+
+    await convertDocs(doc.output);
   });
+}
+
+async function convertDocs(dir) {
+  const docFiles = await readdir(dir);
+  for (const docFile of docFiles) {
+    try {
+      const { name: id, ext } = parse(docFile);
+      if (ext !== ".md") {
+        continue;
+      }
+
+      const docPath = join(dir, docFile);
+      const input = createReadStream(docPath);
+      const output = [];
+      const lines = createInterface({
+        input,
+        crlfDelay: Infinity,
+      });
+
+      let title = "";
+      lines.on("line", (line) => {
+        let skip = false;
+        if (!title) {
+          const titleLine = line.match(/## (.*)/);
+          if (titleLine) {
+            title = titleLine[1];
+          }
+        }
+        const homeLink = line.match(/\[Home\]\(.\/index\.md\) &gt; (.*)/);
+        if (homeLink) {
+          // Skip the breadcrumb for the toplevel index file.
+          if (id !== "faastjs") {
+            output.push(homeLink[1]);
+          }
+          skip = true;
+        }
+        // See issue #4. api-documenter expects \| to escape table
+        // column delimiters, but docusaurus uses a markdown processor
+        // that doesn't support this. Replace with an escape sequence
+        // that renders |.
+        if (line.startsWith("|")) {
+          line = line.replace(/\\\|/g, "&#124;");
+        }
+        if (!skip) {
+          output.push(line);
+        }
+      });
+
+      await new Promise((resolve) => lines.once("close", resolve));
+      input.close();
+
+      const header = [
+        "---",
+        `id: ${id}`,
+        `title: ${title}`,
+        `hide_title: true`,
+        "---",
+      ];
+
+      await writeFile(docPath, header.concat(output).join("\n"));
+    } catch (err) {
+      console.error(`Could not process ${docFile}: ${err}`);
+    }
+  }
 }
 
 main();
