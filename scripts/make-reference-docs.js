192a05f849cfdad2b9280c55a44db8beb862133c
Shawn Mclean <shawnmclean@users.noreply.github.com>
2021-08-20 03:11:07 -0400

    Add SDKs docs (#31)
    * Remove sections.
    
    * Add SDK docs.
    
    * Restructure sidebar.
    
    * Remove link to itself.
    
    * Rename mdx to md.
    
    * Add script to build references.
    
    * Add generated docs to reference directory.
    
    * make-reference converts markdown to have correct headers.
    
    * Add better page name
    
    * Add samelogic-js instead of index to sidebar.
    
    * Update package.
    
    * Update reference docs.
    
    * Add advanced docs.

---
+++ scripts/make-reference-docs.js
@@ -0,0 +1,106 @@
+const { readdir, createReadStream, writeFile } = require("fs-extra");
+const { createInterface } = require("readline");
+const { join, parse } = require("path");
+const { exec } = require("child_process");
+
+// This scripot is used to generate the API documentation for libraries
+// generated with the @microsoft/api-generator library.
+// This script takes inspiration from: https://github.com/faastjs/faast.js/blob/master/build/make-docs.js
+
+const jsonDocs = [
+  {
+    input: "node_modules/@samelogic/samelogic-js/dist",
+    output: "docs/developers/sdks/web/reference",
+  },
+];
+
+async function main() {
+  jsonDocs.forEach(async (doc) => {
+    console.log(`Generating docs for ${doc.input}`);
+    await new Promise((resolve, reject) =>
+      exec(
+        `api-documenter markdown --input-folder ${doc.input} --output-folder ${doc.output}`,
+        (err, stdout, stderr) => {
+          console.log(stdout);
+          console.error(stderr);
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      )
+    );
+
+    await convertDocs(doc.output);
+  });
+}
+
+async function convertDocs(dir) {
+  const docFiles = await readdir(dir);
+  for (const docFile of docFiles) {
+    try {
+      const { name: id, ext } = parse(docFile);
+      if (ext !== ".md") {
+        continue;
+      }
+
+      const docPath = join(dir, docFile);
+      const input = createReadStream(docPath);
+      const output = [];
+      const lines = createInterface({
+        input,
+        crlfDelay: Infinity,
+      });
+
+      let title = "";
+      if (id === "samelogic-js") {
+        title = "Javascript SDK Reference";
+      }
+      lines.on("line", (line) => {
+        let skip = false;
+        if (!title) {
+          const titleLine = line.match(/## (.*)/);
+          if (titleLine) {
+            title = titleLine[1];
+          }
+        }
+        const homeLink = line.match(/\[Home\]\(.\/index\.md\) &gt; (.*)/);
+        if (homeLink) {
+          // Skip the breadcrumb for the toplevel index file.
+          if (id !== "samelogic-js") {
+            output.push(homeLink[1]);
+          }
+          skip = true;
+        }
+        // See issue #4. api-documenter expects \| to escape table
+        // column delimiters, but docusaurus uses a markdown processor
+        // that doesn't support this. Replace with an escape sequence
+        // that renders |.
+        if (line.startsWith("|")) {
+          line = line.replace(/\\\|/g, "&#124;");
+        }
+        if (!skip) {
+          output.push(line);
+        }
+      });
+
+      await new Promise((resolve) => lines.once("close", resolve));
+      input.close();
+
+      const header = [
+        "---",
+        `id: ${id}`,
+        `title: ${title}`,
+        `hide_title: true`,
+        "---",
+      ];
+
+      await writeFile(docPath, header.concat(output).join("\n"));
+    } catch (err) {
+      console.error(`Could not process ${docFile}: ${err}`);
+    }
+  }
+}
+
+main();
--- sidebars.js
+++ sidebars.js
@@ -1,26 +1,54 @@
 module.exports = {
   someSidebar: {
-    Introduction: ['overview/getting-started'],
-    Tutorials: ['tutorials/getting-started', 'tutorials/churn/index'],
-    "How To Guides": ['sections/howto/getting-started'],
-    Workflows: [
-      'workflows/overview',
+    Introduction: ["overview/getting-started"],
+    Developers: [
+      "developers/overview",
       {
-        Triggers: [
-          'workflows/triggers/overview',
-          'workflows/triggers/filters', 
-          'workflows/triggers/page_view',
-          'workflows/triggers/click_on',
-          'workflows/triggers/hover_on'
-        ]
-      }
-    ],
-    Microsurveys: [
-      'microsurveys/overview', 
-      {
-        Fields: ['microsurveys/fields/long_text', 'microsurveys/fields/multiple_choice']
+        type: "category",
+        label: "SDKs",
+        collapsed: false,
+        items: [
+          {
+            type: "category",
+            label: "Javascript",
+            collapsed: false,
+            items: [
+              "developers/sdks/web/introduction",
+              "developers/sdks/web/advanced",
+              "developers/sdks/web/reference/samelogic-js",
+              {
+                type: "link",
+                label: "Releases",
+                href: "https://github.com/Samelogic/samelogic.js/releases",
+              },
+            ],
+          },
+        ],
       },
     ],
-    "Release Notes": ['sections/release_notes/releases']
+    Tutorials: ["tutorials/getting-started", "tutorials/churn/index"],
+    "How To Guides": ["howto/getting-started"],
+    Workflows: [
+      "workflows/overview",
+      {
+        Triggers: [
+          "workflows/triggers/overview",
+          "workflows/triggers/filters",
+          "workflows/triggers/page_view",
+          "workflows/triggers/click_on",
+          "workflows/triggers/hover_on",
+        ],
+      },
+    ],
+    Microsurveys: [
+      "microsurveys/overview",
+      {
+        Fields: [
+          "microsurveys/fields/long_text",
+          "microsurveys/fields/multiple_choice",
+        ],
+      },
+    ],
+    "Release Notes": ["release_notes/releases"],
   },
 };
