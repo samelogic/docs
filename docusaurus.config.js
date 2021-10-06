874154f8c627bd1a7bca46f78ccca6a6e696f9df
Shawn Mclean <shawnrmclean@gmail.com>
2021-10-06 18:45:54 -0500

    Update images.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,15 @@ module.exports = {
   projectName: "Samelogic", // Usually your repo name.
   plugins: [
     "@docusaurus/theme-live-codeblock",
-    "@docusaurus/plugin-ideal-image",
+    [
+      "@docusaurus/plugin-ideal-image",
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+      },
+    ],
   ],
   themeConfig: {
     gtag: {
