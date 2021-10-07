6bdf68258fa14541c8cb241416b241cc9c854745
Shawn Mclean <shawnrmclean@gmail.com>
2021-10-06 22:54:09 -0500

    Update workflow docs.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,8 +15,8 @@ module.exports = {
       "@docusaurus/plugin-ideal-image",
       {
         quality: 70,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
+        max: 640, // max resized image's size.
+        min: 400, // min resized image's size. if original is lower, use that size.
         steps: 2, // the max number of images generated between min and max (inclusive)
       },
     ],
